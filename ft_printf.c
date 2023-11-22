2e4853705de63bcd8d1d9e00f17e18da4d535fc6
Iris Sanders <irissanders@live.nl>
2023-11-22 13:56:38 +0100

    started ft_printf

---
+++ ft_printf.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irsander <irsander@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 23:07:02 by irsander          #+#    #+#             */
+/*   Updated: 2023/11/21 23:14:36 by irsander         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int    ft_printf(const char *, ...)
+{
+	
+}
+
+/* Allowed functions:		Implement following conversions:
+- malloc					- %c prints single character
+- free						- %s Prints a string
+- write						- %p The void * pointer argument has to be printed in hexadecimal format
+- va_start					- %d Prints a decimal (base 10) number
+- va_arg					- %i Prints an integer in base 10
+- va_copy					- %u Prints an unsigned decimal (base 10) number
+- va_end					- %x Prints a number in hexadecimal (base 16) lowercase format
+							- %X Prints a number in hexadecimal (base 16) uppercase format
+							- %% Prints a percent sign
+*/ 
